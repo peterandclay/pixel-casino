4169128ba04e7edb56c08b577c8cba58e1ff49b1
powerc9000 <clay.murray8@gmail.com>
2014-07-29 23:30:44 -0600

    started on engine work

--- src/engine.js
+++ src/engine.js
@@ -1,1 +1,24 @@
 var $h = require("../lib/headOn.js");
+
+(function(){
+	function engine(){
+		if ( engine.prototype._singletonInstance ) {
+		    return engine.prototype._singletonInstance;
+		}
+		engine.prototype._singletonInstance = this;
+		return this;
+	}
+
+	engine.prototype.getInstance = function(){
+		return this.instance;
+	}
+	engine.prototype.init = function(width, height){
+		this.gameWidth = width;
+		this.gameHeight = height;
+		this.camera = new $h.Camera(width, height);
+		this.mainCanvas = $h.canvas.create("main", width, height, this.camera);
+		this.mainCanvas.append("body")
+	}
+	var instance = new engine();
+	module.exports = engine;
+}())
--- src/game.js
+++ src/game.js
@@ -1,1 +1,7 @@
 var $h = require("../lib/headOn.js");
+var engine = require("./engine.js")();
+
+
+engine.init(window.innerWidth, window.innerHeight);
+
+engine.mainCanvas.drawRect(window.innerWidth, window.innerHeight, 0,0, "black")

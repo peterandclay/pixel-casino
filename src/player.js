4f8909787aaa63b51e52277cbb3787c5261d5de3
Peter <peter.jacobsen55@gmail.com>
2014-08-12 23:25:08 -0600

    Update

--- src/player.js
+++ src/player.js
@@ -12,18 +12,20 @@ util.Class(Player, Entity, {
 		var delta = delta/1000;
 		this.dx = this.dy = 0;
 		if(engine.controls.up)
-			this.dy = -1;
-		if(engine.controls.down)
 			this.dy = 1;
+		if(engine.controls.down)
+			this.dy = -1;
 		if(engine.controls.left)
-			this.dx = -1;
-		if(engine.controls.right)
 			this.dx = 1;
+		if(engine.controls.right)
+			this.dx = -1;
 		
 
 
 		this.pos.x += this.dx * delta * 100;
 		this.pos.y += this.dy * delta * 100;
+
+		engine.camera.moveTo(this.pos);
 
 	},
 	render: function(canvas){
